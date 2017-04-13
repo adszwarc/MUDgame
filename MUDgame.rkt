09ccfcbe3fa6795b73af1378b07adb38e9c01171
Adrian D. Szwarc <21298244@student.uwl.ac.uk>
2017-04-13 08:48:19 +0100

    v.0.0.1
    Defining room descriptions and the object

--- MUDgame.rkt
+++ MUDgame.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+(require srfi/1)
+(require srfi/13)
+
+;; specifying the objects
+(define objects `((1 "a short sword")
+                  (1 "a shapphire")))
+
+;; defining room descriptions
+(define descriptions `((1 "You are in the chamber.")
+                       (2 "You are in the corridor.")
+                       (3 "You are in the great hall.")
+                       (4 "You are in a storeroom.")))
