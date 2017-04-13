2714faa79c8c8f64db2129f3ee990ce629767277
Adrian D. Szwarc <21298244@student.uwl.ac.uk>
2017-04-13 09:17:57 +0100

    v.0.0.2
    defining actions and decisiontable structure

--- MUDgame.rkt
+++ MUDgame.rkt
@@ -3,12 +3,28 @@
 (require srfi/1)
 (require srfi/13)
 
-;; specifying the objects
-(define objects `((1 "a short sword")
-                  (1 "a shapphire")))
+;; specifying objects descriptions
+(define objects '((1 "a short sword")
+                  (1 "a shapphire key")))
 
 ;; defining room descriptions
-(define descriptions `((1 "You are in the chamber.")
+(define descriptions '((1 "You are in the chamber.")
                        (2 "You are in the corridor.")
                        (3 "You are in the great hall.")
-                       (4 "You are in a storeroom.")))
+                       (4 "You are in a chapel.")
+                       (5 "You are in a storeroom.")
+                       (6 "You are in secret room."))
+
+;; defining actions
+(define look '(((directions) look) ((look) look) ((examine room) look)))
+(define quit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
+(define pick '(((get) pick) ((pickup) pick) ((pick) pick)))
+(define drop '(((put) drop) ((drop) drop) ((place) drop) ((remove) drop)))
+(define inventory '(((inventory) inventory) ((bag) inventory)))
+(define actions `(,@look ,@quit ,@pick ,@put ,@inventory))
+
+;; defining decisiontable
+(define decisiontable `((1 ((east) 2) ,@actions)
+                        (2 ((west) 1) ((east) 2) ,@actions)
+                        (3 ((west) 2) ((north) 5) ((south) 4) ,@actions)))
+  
